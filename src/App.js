55f050a385c91bf816ccd4268cb581994733d5d9
Faisal <apple@apples-MacBook-Pro-2.local>
2022-09-08 15:45:37 +0500

    first v

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import LoginButton from "./Login";
+import LogoutButton from "./Logout";
+import Profile from "./Profile";
+import Auth0ProviderWithHistory from "./Auth0Provider";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Auth0ProviderWithHistory>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          React Auth0
+          <br />
+          <br />
+          <LoginButton />
+          <LogoutButton />
+          <Profile />
+        </header>
+      </div>
+    </Auth0ProviderWithHistory>
   );
 }
 
